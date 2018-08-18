bacd6fd4ae46c10f351835e5f2b394d4957b47c6
DESKTOP-4PAEVK3\Rima <rima.lewis20@gmail.com>
2018-08-19 00:08:48 +0200

    minor adjustments

--- src/app/components/radarChart/radarChart.controller.js
+++ src/app/components/radarChart/radarChart.controller.js
@@ -1,9 +1,11 @@
-import {assign} from 'lodash';
+import {assign,forEach} from 'lodash';
 
 class radarChartController {
 	constructor($scope, normalizedValuesService,d3Service, $log, $timeout){
 		assign(this, {$scope, normalizedValuesService,d3Service, $log, $timeout});
 	}
+
+
 
 	$onInit() {
 		this.dynamicExpression = 'from the parent controller';
@@ -12,11 +14,47 @@ class radarChartController {
 		this.d3 = this.d3Service.getD3();
 
 
+		Highcharts.wrap(Highcharts.Series.prototype, 'drawPoints', function(p) {
+			const options = this.options;
+			const symbolCallback = options.marker && options.marker.symbolCallback;
+			const points = this.points;
+
+			if (symbolCallback && points.length) {
+				points.forEach(point => {
+					const symbol = symbolCallback.call(point);
+					if (symbol) {
+						point.update({
+							marker: {
+								fillColor: '#FFFFFF',
+								radius: 5,
+								lineWidth: 2,
+								lineColor: null // inherit from series
+							}
+						}, false);
+					}
+				});
+			}
+
+			p.call(this);
+		});
+
+
+
 		Highcharts.chart('container', {
 
 			chart: {
-				polar: true
+				polar: true,
+				animation: true,
 			},
+
+			loading: {
+				hideDuration: 500,  // The duration in milliseconds of the fade out effect.
+				labelStyle: null,   // CSS styles for the loading label span.
+				showDuration: 500,  // The duration in milliseconds of the fade in effect.
+				style: null         // CSS styles for the loading screen that covers the plot area.
+			},
+
+
 
 			title: {
 				text: 'Budget vs spending',
@@ -28,6 +66,8 @@ class radarChartController {
 			},
 
 			xAxis: {
+				gridLineColor: '#a2aba0',
+				// gridLineDashStyle: 'dash',
 				categories: ['Sales', 'Marketing', 'Development', 'Customer Support',
 					'Information Technology', 'Administration', 'new'],
 				tickmarkPlacement: 'on',
@@ -36,9 +76,20 @@ class radarChartController {
 
 
 			yAxis: {
+				gridLineColor: '#a2aba0',
+				gridLineDashStyle: 'dash',
 				gridLineInterpolation: 'circle',
 				lineWidth: 1,
-				min: -1
+				min: -1,
+				plotBands: [{
+					color: '#ffc0cb',
+					from: 0,
+					to: -1
+				},{
+					color: '#b8eab8',
+					from: 0,
+					to: +1
+				}],
 			},
 
 			tooltip: {
@@ -55,11 +106,23 @@ class radarChartController {
 
 			series: [{
 				name: 'Allocated Budget',
-				data: [-0.1,-0.56, -0.45, -0.7, -0.4, -0.3,0.7],
-				pointPlacement: 'on'
+				marker: {
+					symbolCallback: function() {
+						if( this.y === 0)
+							return 'circle';
+					}
+				},
+				data: [-0.1,0.0, -0.45, -0.7, -0.4, -0.3,0.7],
+				pointPlacement: 'on',
 			}, {
 				name: 'Actual Spending',
-				data: [0.4, 0.4, 0.6, 0.3, 0.6, 0.23,0.8],
+				marker: {
+					symbolCallback: function() {
+						if( this.y === 0)
+							return 'circle';
+					}
+				},
+				data: [0.4, 0.4, 0.6, 0.3, 0.6, 0.02,0.0],
 				pointPlacement: 'on'
 			}]
 
--- src/app/components/textplot/textPlot.controller.js
+++ src/app/components/textplot/textPlot.controller.js
@@ -11,6 +11,31 @@ class textPlotController {
 		this.in = 'from the parent controller';
 		const data = this.normalizedValuesService.getNormalizedValues();
 
+		Highcharts.wrap(Highcharts.Series.prototype, 'drawPoints', function(p) {
+			const options = this.options;
+			const symbolCallback = options.marker && options.marker.symbolCallback;
+			const points = this.points;
+
+			if (symbolCallback && points.length) {
+				points.forEach(point => {
+					const symbol = symbolCallback.call(point);
+					if (symbol) {
+						point.update({
+							marker: {
+								fillColor: '#FFFFFF',
+								radius: 5,
+								lineWidth: 2,
+								lineColor: null // inherit from series
+							}
+						}, false);
+					}
+				});
+			}
+
+			p.call(this);
+		});
+
+
 		Highcharts.chart('container2',{
 
 			chart: {
@@ -24,6 +49,8 @@ class textPlotController {
 			},
 
 			xAxis: [{
+				gridLineColor: '#a2aba0',
+				gridLineDashStyle: 'dash',
 				categories: ['Sales', 'Marketing', 'Development', 'Customer Support', 'Information Technology', 'Administration', 'new'],
 				tickmarkPlacement: 'on',
 			}, {
@@ -34,13 +61,23 @@ class textPlotController {
 			}],
 
 			yAxis: {
-				gridLineInterpolation: 'circle',
+				gridLineColor: '#a2aba0',
+				gridLineDashStyle: 'dash',
 				lineWidth: 1,
-				min: -1
+				min: -1,
+				plotBands: [{
+					color: '#f9a6a6',
+					from: 0,
+					to: -1
+				},{
+					color: '#b4f1a2',
+					from: 0,
+					to: +1
+				}],
 			},
 
 			tooltip: {
-				shared: true,
+				shared: false,
 				pointFormat: '<span style="color:{series.color}">{series.name}: <b>{point.y}</b><br/>'
 			},
 
@@ -53,11 +90,23 @@ class textPlotController {
 
 			series: [{
 				name: 'Allocated Budget',
-				data: [-0.1,-0.56, -0.45, -0.7, -0.4, -0.3,0.7],
+				marker: {
+					symbolCallback: function() {
+						if( this.y === 0)
+							return 'circle';
+					}
+				},
+				data: [-0.1,-0.0, -0.45, -0.0001, -0.4, -0.3,0.7],
 				pointPlacement: 'on'
 			}, {
 				name: 'Actual Spending',
-				data: [0.4, 0.4, 0.6, 0.3, 0, 0.23,0.8],
+				marker: {
+					symbolCallback: function() {
+						if( this.y === 0)
+							return 'circle';
+					}
+				},
+				data: [0.4, 0.4, 0.6, 0.3, 0, 0.92,0.3],
 				pointPlacement: 'on'
 			}]
 
